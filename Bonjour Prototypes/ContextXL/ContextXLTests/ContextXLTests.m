38cebab85ebfd2d6bc760772e40e2ac5532eb94d
zanotti <zanotti@adobe.com>
2013-10-16 15:57:11 -0700

    Second Commit

---
+++ Bonjour Prototypes/ContextXL/ContextXLTests/ContextXLTests.m
@@ -0,0 +1,36 @@
+//
+//  ContextXLTests.m
+//  ContextXLTests
+//
+//  Created by zanotti on 9/30/13.
+//  Copyright (c) 2013 zanotti. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ContextXLTests : XCTestCase
+
+@end
+
+@implementation ContextXLTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
